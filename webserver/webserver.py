0334a0f1161876ca0d897b7fb863baf2e25afe56
Ankit Rajpoot <anktir.42@gmail.com>
2018-10-27 04:52:10 +0530

    Added basic authentication to webserver.

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -1,16 +1,43 @@
 import web
+import re
+import base64
 
-urls = ('/', 'index', '/logs', 'logs')
+urls = ('/', 'Index', '/logs', 'Logs', '/login', 'Login')
 html = web.template.render('webtemplates')
 json = web.template.render('../cache')
+users = ( ('admin', 'admin') )
 
-class index:
-    def GET(self):
-        return html.index()
-
-class logs:
+class Index:
 	def GET(self):
-		return json.logs()
+		if web.ctx.env.get('HTTP_AUTHORIZATION') is None:
+			web.seeother('/login')
+		else:
+			html.index()
+
+class Logs:
+	def GET(self):
+		if web.ctx.env.get('HTTP_AUTHORIZATION') is None:
+			web.seeother('/login')
+		else:
+			return json.logs()
+
+class Login:
+    def GET(self):
+        auth = web.ctx.env.get('HTTP_AUTHORIZATION')
+        authreq = False
+        if auth is None:
+            authreq = True
+        else:
+            auth = re.sub('^Basic ','',auth)
+            username,password = base64.decodestring(auth).split(':')
+            if (username,password) in users:
+                raise web.seeother('/')
+            else:
+                authreq = True
+        if authreq:
+            web.header('WWW-Authenticate','Basic realm="Auth example"')
+            web.ctx.status = '401 Unauthorized'
+            return
 
 def startServer():
     app = web.application(urls, globals())
