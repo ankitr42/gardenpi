48026eb2e0bb8effb10611c948fb4a7611fb75f2
Ankit Rajpoot <ankitr.42@gmail.com>
2018-11-01 02:57:08 +0530

    Add comments to charge controller.

--- chargecontroller.py
+++ chargecontroller.py
@@ -9,22 +9,25 @@ import db
 
 from consts import CHARGER_GPIO
 from consts import CHARGER_RUNTIME
-from consts import CHARGER_RUN_INTERVAL
+from consts import CHARGER_OFFTIME
 
 chargeRelay = gpiozero.OutputDevice(CHARGER_GPIO, active_high = False,
                                   initial_value = False)
                                   
 chargeState = False
 while True:
+    # Start charging
     chargeRelay.on()
-    
     entry = { 'timestamp': str(datetime.now().timestamp()),
                 'event': 'Switch On'}
     db.logChargeRun(entry)
 
     time.sleep(CHARGER_RUNTIME)
 
+    # Stop charging charging
     chargerRelay.off()
     entry = { 'timestamp': str(datetime.now().timestamp()),
                 'event': 'Switch Off'}
     db.logChargeRun(entry)
+
+    time.sleep(CHARGER_OFFTIME)
